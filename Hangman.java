9494e504b78400ed517cce562ec8749f03982743
Wisertown <aaron.wise253@gmail.com>
2016-01-09 09:49:09 -0500

    first commit

---
+++ Hangman.java
@@ -0,0 +1,15 @@
+public class Hangman {
+	
+	public static void main(String[] args){
+	
+		Game game = new Game("TOby");
+		Prompter prompter = new Prompter(game);
+		boolean isHit = prompter.promptForGuess();
+		if(isHit){
+			System.out.println("we got a hit");
+		} else {
+			System.out.println("That was a miss");
+		}
+	}
+
+}
